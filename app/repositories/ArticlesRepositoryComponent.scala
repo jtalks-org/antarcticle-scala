2628d8efa3ddb07f6f7297a51cec0bfcbbff0038
wedens <wedens13@yandex.ru>
2014-01-05 23:58:25 +0700

    articles repository refactoring

--- app/repositories/ArticlesRepositoryComponent.scala
+++ app/repositories/ArticlesRepositoryComponent.scala
@@ -23,34 +23,38 @@ trait SlickArticlesRepositoryComponent extends ArticlesRepositoryComponent {
 
   val articlesRepository = new SlickArticlesRepository
 
-  class SlickArticlesRepository extends ArticlesRepository {
-    import profile.simple._
+  import profile.simple._
 
-    def getList(offset: Int, portionSize: Int)(implicit s: Session) = {
-      //TODO: remove map(withTags)
-      articlesWithAuthor
+  implicit class ArticlesExtension[E](val q: Query[Articles.type, E]) {
+    //TODO: simplify with slick 2.0
+    def withAuthor(u: Query[Users.type, UserRecord] = Query(Users)): Query[(Articles.type, Users.type), (E, UserRecord)] = {
+      q.leftJoin(u).on(_.authorId === _.id)
+    }
+
+    def portion(offset: Int, portionSize: Int) = {
+      q.withAuthor()
         .drop(offset)
         .take(portionSize)
         .sortBy { case (article, _) => article.createdAt }
-        .list.map(withTags)
+    }
+
+    def byId(id: Column[Int]): Query[Articles.type, E] = {
+      q.filter(_.id === id)
+    }
+  }
+
+  class SlickArticlesRepository extends ArticlesRepository {
+
+    def getList(offset: Int, portionSize: Int)(implicit s: Session) = {
+      Query(Articles).portion(offset, portionSize).list.map(fetchTags)
     }
 
     def getListForUser(userId: Int, offset: Int, portionSize: Int)(implicit s: Session) = {
-      //TODO: remove map(withTags) and remove duplication with getList
-      (for {
-        article <- Articles
-        author <- article.author if author.id === userId
-      } yield (article, author))
-      .drop(offset)
-      .take(portionSize)
-      .sortBy { case (article, _) => article.createdAt }
-      .list.map(withTags)
+      Query(Articles).filter(_.authorId === userId).portion(offset, portionSize).list.map(fetchTags)
     }
 
     def get(id: Int)(implicit s: Session) = {
-      (for {
-        (article, author) <- articlesWithAuthor if article.id === id
-      } yield (article, author)).firstOption.map(withTags)
+      Query(Articles).byId(id).withAuthor().firstOption.map(fetchTags)
     }
 
     def insert(article: ArticleToInsert)(implicit s: Session) = {
@@ -58,14 +62,11 @@ trait SlickArticlesRepositoryComponent extends ArticlesRepositoryComponent {
     }
 
     def update(id: Int, articleToUpdate: ArticleToUpdate)(implicit s: Session) = {
-      Articles
-        .filter(_.id === id)
-        .map(_.forUpdate)
-        .update(articleToUpdate) > 0
+      Query(Articles).byId(id).map(_.forUpdate).update(articleToUpdate) > 0
     }
 
     def remove(id: Int)(implicit s: Session) = {
-      Articles.where(_.id === id).delete > 0
+      Query(Articles).byId(id).delete > 0
     }
 
     def count()(implicit s: Session) = {
@@ -73,32 +74,20 @@ trait SlickArticlesRepositoryComponent extends ArticlesRepositoryComponent {
     }
 
     def countForUser(userId: Int)(implicit s: Session) = {
+      //TODO: replace count with something else in slick 2.0
       (for {
         article <- Articles if article.authorId === userId
       } yield article.id.count).first
     }
 
-    //TODO: join tags in the same slick query as article
-    private def withTags(t: (ArticleRecord, UserRecord))(implicit s: Session) = t match {
-      case (article, author) => (article, author, getArticleTagsNames(article.id.get))
+    private def fetchTags(t: (ArticleRecord, UserRecord))(implicit s: Session) = t match {
+      case (article, author) => (article, author, articleTags(article.id.get).list)
     }
 
-    private def getArticleTagsNames(articleId: Int)(implicit s: Session) = {
-      articleTags(articleId).list.map {
-        case (_, name) => name
-      }
-    }
-
-    //TODO: convert to val (compiled query)?
-    private def articlesWithAuthor = for {
-        article <- Articles
-        author <- article.author
-      } yield (article, author)
-
-    //TODO: convert to val (compiled query)?
+    //TODO: convert to compiled query?
     private def articleTags(articleId: Int) = for {
         articleTag <- ArticlesTags if articleTag.articleId === articleId
         tag <- Tags if articleTag.tagId === tag.id
-      } yield tag
+      } yield tag.name
   }
 }

a30df7f105e53d454f5b3c38f94e2fbec425ce70
Jk1 <eugenbox@gmail.com>
2014-01-12 07:58:46 +0400

    Removed unnecessary migration exception handling

--- app/migrations/Migrations.scala
+++ app/migrations/Migrations.scala
@@ -5,18 +5,14 @@ import scala.slick.jdbc.{GetResult, StaticQuery => Q}
 import scala.slick.session.Session
 
 class Migrations(profile: ExtendedProfile) extends MigrationsContainer {
+
   import profile.simple._
 
   val addRememberMeTokenToUser = new Migration {
     val version = 1
 
     def run(implicit session: Session): Unit = {
-      try {
-        Q.updateNA("alter table users add remember_token varchar(64)").execute
-      }
-      catch {
-        case  e:Exception => // it seems that column is already in place, just skip this migration
-      }
+      Q.updateNA("alter table users add remember_token varchar(64)").execute
     }
   }
 

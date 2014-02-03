04e953feb800bceca398b8fd690b9ed07edbe49f
wedens <wedens13@yandex.ru>
2014-02-04 01:15:48 +0700

    ANTARCTICLE-171 fenced code blocks with specified language handling

--- test/MarkdownSpec.scala
+++ test/MarkdownSpec.scala
@@ -3,16 +3,31 @@ import views.helpers.Markdown
 
 class MarkdownSpec extends Specification{
 
-  // "markdown" should {
-  //   "create code tag with explicit language" in {
-  //     val fencedCodeBlockWithExplicitLang = """```ruby
-  //     require 'redcarpet'
-  //     markdown = Redcarpet.new("Hello World!")
-  //     puts markdown.to_html
-  //     ```
-  //     """
+  "markdown to html" should {
+    "produce extended syntax highlighting capable html" in {
+      val markdownSource = """
+```scala
+class Example(name: String) {
+    val field: Option[Int] = None
+}
+```
+"""
+      val expectedCodeTag = "<code class=\"lang-scala\">"
 
-  //     Markdown.toHtml(fencedCodeBlockWithExplicitLang) must_== "lalal"
-  //   }
-  // }
+      Markdown.toHtml(markdownSource) must contain(expectedCodeTag)
+    }
+
+    "produce html for fenced code blocks without explicit language" in {
+      val markdownSource = """
+```
+class Example(name: String) {
+    val field: Option[Int] = None
+}
+```
+"""
+      val expectedCodeTag = "<code>"
+
+      Markdown.toHtml(markdownSource) must contain(expectedCodeTag)
+    }
+  }
 }

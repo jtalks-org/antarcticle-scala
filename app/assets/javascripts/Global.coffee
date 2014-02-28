d00bb3b044a267fa0af31dc638b638bf1698d44f
Jk1 <eugenbox@gmail.com>
2014-02-28 09:24:15 +0400

    ANTARCTICLE-196 code highlight for markdown

--- app/assets/javascripts/Global.coffee
+++ app/assets/javascripts/Global.coffee
@@ -1,10 +1,5 @@
 jQuery(=>
   $(document).ready(=>
-    $('pre > code').each (i, el) =>
-      # add classes for highlighting and line numbers
-      $(el).parent().addClass('prettyprint').addClass('linenums')
-    # turn on highlighting
-    prettyPrint()
     # custom tags input field
     $('#tags_filter').tags_input()
   )
--- app/views/helpers/Markdown.scala
+++ app/views/helpers/Markdown.scala
@@ -2,61 +2,31 @@ package views.helpers
 
 import org.pegdown._
 import org.pegdown.Extensions._
-import org.parboiled.common.StringUtils
 import org.pegdown.ast._
 import org.apache.commons.lang3.StringEscapeUtils
 
 /**
- * Adds class with specified in fenced code block language for
- * extended syntax highlighting using google prettify.
+ * <p>Extends PegDown serializer to archive the following:
  *
- * Example:
- * Should produce: <pre><code class="lang-ruby">..</code></pre>
- * for markdown like:
- * ```ruby
- * ruby code here
- * ```
- */
-object GooglePrettifyVerbatimSerializer extends VerbatimSerializer {
-
-  val map  = new java.util.HashMap[String, VerbatimSerializer]
-  map.put(VerbatimSerializer.DEFAULT, GooglePrettifyVerbatimSerializer)
-  
-  override def serialize(node: VerbatimNode, printer: Printer): Unit = {
-    printer.println().print("<pre><code")
-    if (!StringUtils.isEmpty(node.getType)) {
-      printAttribute(printer, "class", s"lang-${node.getType}")
-    }
-    printer.print(">")
-    var text = node.getText
-    // print HTML breaks for all initial newlines
-    while (text.charAt(0) == '\n') {
-      printer.print("<br/>")
-      text = text.substring(1)
-    }
-    printer.printEncoded(text)
-    printer.print("</code></pre>")
-
-  }
-
-  private def printAttribute(printer: Printer, name: String, value: String): Unit = {
-    printer.print(' ').print(name).print('=').print('"').print(value).print('"')
-  }
-}
-
-/**
- * Prevents XSS attack by escaping any tags in article/comment contents,
+ * <p>1. Prevents XSS attack by escaping any tags in article/comment contents,
  * which are not parsed as explicit code by markdown parser
+ * <p>2. Adds source code formatting CSS classes
  */
-class EscapingToHtmlSerializer extends ToHtmlSerializer(new LinkRenderer, GooglePrettifyVerbatimSerializer.map){
+class EscapingToHtmlSerializer extends ToHtmlSerializer(new LinkRenderer){
 
   override def visit(node : HtmlBlockNode) {
     val text = node.getText
     if (text.length() > 0) printer.println()
-    printer.print(escape(text))
+    printer.printEncoded(text)
   }
 
-  override def visit(node : InlineHtmlNode ) = printer.print(escape(node.getText))
+  override def visit(node : InlineHtmlNode ) = printer.printEncoded(node.getText)
+
+  override def visit(node: CodeNode) = {
+    printer.println().print("<pre class='prettyprint linenums'><code>")
+    printer.printEncoded(node.getText)
+    printer.print("</code></pre>")
+  }
 
   private def escape(text:String) = StringEscapeUtils.escapeHtml4(text)
 }

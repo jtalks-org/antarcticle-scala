befda71a6fba90c81bde9643148f68cf8ecbe6ad
Jk1 <eugenbox@gmail.com>
2014-04-27 00:57:44 +0400

    ANTARCTICLE-249 correct notification removal

--- app/assets/javascripts/NotificationsSupport.coffee
+++ app/assets/javascripts/NotificationsSupport.coffee
@@ -1,8 +1,17 @@
 jQuery(=>
   $(document).ready(=>
-    $.get($("#notifications-link").attr('href'))
-    .done((data) =>
+    $.get($("#notifications-link").attr('href')).done((data) =>
       $("#notifications-dropdown").html(data)
+      $("#drop-notifications-button").click((e) ->
+        $.ajax({
+          url: $(this).attr('href'),
+          type: 'DELETE'
+        }).done((data) =>
+          $("#notifications-dropdown").html(data)
+        )
+        e.preventDefault()
+        return false
+      )
     )
   )
 )
--- app/controllers/NotificationsController.scala
+++ app/controllers/NotificationsController.scala
@@ -4,33 +4,37 @@ import play.api.mvc.{Action, Controller}
 import security.Authentication
 import models.database.Notification
 import java.sql.Timestamp
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 /**
- *
+ *   Temporary notification controller implementation to be used until
+ *   proper backend for notifications is implemented
  */
 trait NotificationsController {
   this: Controller with Authentication =>
 
-  def getNotifications() = Action {
+  var data = ArrayBuffer[Notification](
+    Notification(Some(1), 1, 1, 1, "Yo mama is so stupid", "she thought a quarterback was a refund", new Timestamp(1)),
+    Notification(Some(2), 1, 2, 1, "Yet another notification", "There are so many of them", new Timestamp(100000000000l)),
+    Notification(Some(3), 1, 2, 1, "These notifications are fake", "User story is not ready yet", new Timestamp(500000000000l))
+  )
+
+  def getNotifications = Action {
     implicit request =>
-      //todo: implement me
-      val data = List(
-        Notification(None, 1, 1, 1, "Yo mama is so stupid", "she thought a quarterback was a refund", new Timestamp(1)),
-        Notification(None, 1, 2, 1, "Yet another notification", "There are so many of them", new Timestamp(100000)),
-        Notification(None, 1, 2, 1, "These notifications are fake", "User story is not ready yet", new Timestamp(500000))
-      )
       Ok(views.html.templates.notifications(data))
   }
 
-  def removeNotification(id: Int) = Action {
+  def getNotification(id: Int) = Action {
     implicit request =>
-      //todo: implement me
-      Ok("")
+      val notification = data.filter((notification) => notification.id == Some(id)).head
+      data = data.filter((notification) => notification.id != Some(id))
+      Found(routes.ArticleController.viewArticle(notification.articleId).absoluteURL() + "#" + notification.commentId)
   }
 
-  def removeAllNotifications() = Action {
+  def removeAllNotifications = Action {
     implicit request =>
-      //todo: implement me
-      Ok("")
+      data.clear()
+      Ok(views.html.templates.notifications(List()))
   }
 }

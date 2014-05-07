1b8f2efd15f6ef5fc6789e0b475f877e6cc927e2
Anuar Nurmakanov <anuar_nurmakanov@epam.com>
2014-05-07 23:13:29 +0600

    ANTARCTICLE-249 test notification controller method which gets the list of notifications

---
+++ test/controllers/NotificationControllerSpec.scala
@@ -0,0 +1,49 @@
+package controllers
+
+import services.NotificationsServiceComponent
+import security.{Principal, Authorities, AuthenticatedUser}
+import java.sql.Timestamp
+import models.database.Notification
+
+import org.specs2.mutable.Specification
+import org.specs2.mock.Mockito
+import util.FakeAuthentication
+import org.specs2.specification.AfterExample
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class NotificationControllerSpec extends Specification with Mockito with AfterExample {
+  object controller extends NotificationsController
+    with NotificationsServiceComponent
+    with FakeAuthentication {
+    override val notificationsService = mock[NotificationsService]
+    override val usersRepository = mock[UsersRepository]
+  }
+
+  import controller._
+
+  def after = {
+    org.mockito.Mockito.reset(usersRepository)
+  }
+
+  val currentUserId = 1
+  val authenticatedUser = new AuthenticatedUser(currentUserId, "user", Authorities.User)
+
+  val time = new Timestamp(System.currentTimeMillis)
+  val notification = Notification(Some(1), 1, 2, 1, "", "", time)
+
+
+  "get notifications" should {
+    "return all exist notifications" in {
+      val notifications = Seq(notification)
+      notificationsService.getNotificationsForCurrentUser(any[Principal]) returns notifications
+
+      val page = controller.getNotifications()(FakeRequest())
+
+      status(page) must equalTo(200)
+      contentType(page) must beSome("text/html")
+      contentAsString(page).contains(notification.title) must beTrue
+      contentAsString(page).contains(notification.content) must beTrue
+    }
+  }
+}

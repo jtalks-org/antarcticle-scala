59c0804f33744f1a929c1f1a5d2de2138fa4d554
Anuar Nurmakanov <anuar_nurmakanov@epam.com>
2014-05-29 21:02:10 +0600

    ANTARCTICLE-276 added service for getting instance name

--- app/services/PropertiesServiceComponent.scala
+++ app/services/PropertiesServiceComponent.scala
@@ -1,5 +1,7 @@
 package services
 
+import repositories.PropertiesRepositoryComponent
+
 trait PropertiesServiceComponent {
   val propertiesService: PropertiesService
 
@@ -11,11 +13,23 @@ trait PropertiesServiceComponent {
 }
 
 trait PropertiesServiceComponentImpl extends PropertiesServiceComponent {
+  this: SessionProvider with PropertiesRepositoryComponent =>
+
   val propertiesService = new PropertiesServiceImpl
 
   class PropertiesServiceImpl extends PropertiesService {
-    def getInstanceName(): String = {
-      ""
+
+    def getInstanceName(): String = withSession {
+      implicit session =>
+        val propertyName = "INSTANCE_NAME"
+        val defaultValue = "ANTARCTICLE"
+        val property = propertiesRepository.getProperty(propertyName)
+
+        property match {
+          case Some(x) =>
+            x.value.getOrElse(defaultValue)
+          case None => defaultValue
+        }
     }
 
     def changeInstanceName(newName: String): Unit = {

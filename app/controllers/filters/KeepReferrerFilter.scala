06104bb5e247c220bde9282ff48e9661df5b9a70
sptp <sporter123@yandex.ru>
2014-06-03 23:36:30 +0600

    ANTARCTICLE-283 Fixed processing requests by KeepRefererFilter.

--- app/controllers/filters/KeepReferrerFilter.scala
+++ app/controllers/filters/KeepReferrerFilter.scala
@@ -9,9 +9,21 @@ import play.mvc.Http.HeaderNames
  *
  */
 object KeepReferrerFilter extends Filter {
+
+  val appliedRoutes = Seq(
+    "/articles",
+    "/users",
+    "/roles",
+    "/help/markdown"
+  )
+
   override def apply(next: (RequestHeader) => Future[SimpleResult])(rh: RequestHeader): Future[SimpleResult] = {
     next(rh).map { result =>
-      result.withSession(HeaderNames.REFERER -> rh.path)
+      rh.method match {
+          case "GET" if appliedRoutes.exists(x => rh.path.startsWith(x)) =>
+                                      result.withSession(HeaderNames.REFERER -> rh.path)
+          case _ => result
+        }
     }
   }
 }

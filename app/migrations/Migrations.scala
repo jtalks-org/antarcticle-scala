b79b22ceeacf7a4c2f7b16c324b54e8320994a06
Evgeny Naumenko <Evgeny.Naumenko@jetbrains.com>
2014-07-01 20:47:46 +0400

    restored old migrations for extended version compatibility

--- app/migrations/Migrations.scala
+++ app/migrations/Migrations.scala
@@ -16,6 +16,49 @@ import scala.slick.jdbc.{StaticQuery => Q, JdbcBackend}
  */
 class Migrations(profile: JdbcProfile) extends MigrationsContainer {
 
+  val addRememberMeTokenToUser = new Migration {
+    val version = 1
+
+    def run(implicit session: JdbcBackend#Session): Unit = {
+      Q.updateNA("alter table users add remember_token varchar(64)").execute
+    }
+  }
+
+  val changeContentColumnTypes = new Migration {
+    val version = 2
+
+    def run(implicit session: JdbcBackend#Session): Unit = {
+      Q.updateNA("alter table articles modify content longtext").execute
+      Q.updateNA("alter table articles modify description longtext").execute
+      Q.updateNA("alter table comments modify content longtext").execute
+    }
+  }
+
+  /**
+   *  Non-null timestamps in MySQL by default are assigned with 'on update CURRENT_TIMESTAMP',
+   *  we obviously don't need it for 'created_at' fields
+   */
+  val removeDefaultOnUpdateConstraintFromTimestampField = new Migration {
+    val version = 3
+
+    def run(implicit session: JdbcBackend#Session): Unit = {
+      Q.updateNA("alter table articles change created_at created_at timestamp not null default current_timestamp").execute
+      Q.updateNA("alter table comments change created_at created_at timestamp not null default current_timestamp").execute
+    }
+  }
+
+  /**
+   * Current description building strategy (cutoff after 300 chars) cannot guarantee proper markup preservation
+   * for the description. Until better content shortening strategy is developed we're temporary setting
+   * description equal to full content.
+   */
+  val setDescriptionToFullContent = new Migration {
+    val version = 4
+
+    def run(implicit session: JdbcBackend#Session): Unit = {
+      Q.updateNA("UPDATE articles SET description = content").execute
+    }
+  }
 
   val addPasswordColumnToUsersTable = new Migration {
     val version = 5
@@ -25,6 +68,8 @@ class Migrations(profile: JdbcProfile) extends MigrationsContainer {
       Q.updateNA("update users set password=''").execute
     }
   }
+
+  // migration 7 is just a rollback of migration 6, so we can safely drop both
 
   val addCommentsNotifications = new Migration {
     val version = 8

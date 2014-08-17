1e183bc2b57707a9bd384932de343b0c8eeca989
jk1 <eugenbox@gmail.com>
2014-08-17 16:47:27 +0400

    ANTARCTICLE-316 replaced align attribute with proper css equiv

--- app/views/helpers/EscapingToHtmlSerializer.scala
+++ app/views/helpers/EscapingToHtmlSerializer.scala
@@ -1,7 +1,8 @@
 package views.helpers
 
+import org.pegdown.ast.TableColumnNode.Alignment
 import org.pegdown.{LinkRenderer, ToHtmlSerializer}
-import org.pegdown.ast.{CodeNode, VerbatimNode, InlineHtmlNode, HtmlBlockNode}
+import org.pegdown.ast._
 import org.apache.commons.lang3.StringUtils
 
 /**
@@ -10,6 +11,7 @@ import org.apache.commons.lang3.StringUtils
  * <p>1. Prevents XSS attack by escaping any tags in article/comment contents,
  * which are not parsed as explicit code by markdown parser
  * <p>2. Adds source code formatting CSS classes
+ * <p>3. Override output for better W3C compliance
  */
 class EscapingToHtmlSerializer extends ToHtmlSerializer(new LinkRenderer){
 
@@ -29,6 +31,13 @@ class EscapingToHtmlSerializer extends ToHtmlSerializer(new LinkRenderer){
     printer.print(">")
     printer.printEncoded(node.getText.replaceAll("\\n\\n","\n").trim)
     printer.print("</code></pre>")
+  }
+
+  override def visit(node: TableColumnNode) {
+    printer.print(node.getAlignment match {
+      case Alignment.None => ""
+      case _ => s" style='text-align:${node.getAlignment.toString.toLowerCase}'"
+    })
   }
 
   override def visit(node: CodeNode): Unit = super.visit(node)
